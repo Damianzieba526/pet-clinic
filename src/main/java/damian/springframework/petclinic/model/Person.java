ae9568969abf98ad27d63494b4d04454545870b0
Damianzieba526 <48860148+Damianzieba526@users.noreply.github.com>
2021-05-05 17:11:40 +0200

    Adding POJO's

---
+++ src/main/java/damian/springframework/petclinic/model/Person.java
@@ -0,0 +1,22 @@
+package damian.springframework.petclinic.model;
+
+public class Person {
+    private String fistName;
+    private String LastName;
+
+    public String getFistName() {
+        return fistName;
+    }
+
+    public void setFistName(String fistName) {
+        this.fistName = fistName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+}
