7b91155eb5d982feda5b8a52f6f0359cb8a2bc30
Damianzieba526 <48860148+Damianzieba526@users.noreply.github.com>
2021-08-18 13:37:22 +0200

    added PetTypeService

---
+++ src/main/java/damian/springframework/petclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package damian.springframework.petclinic.services;
+
+import damian.springframework.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType,Long>{
+
+}
