7f6a39857980b4eda869a5879824071f91afe827
Damianzieba526 <48860148+Damianzieba526@users.noreply.github.com>
2021-08-19 10:00:03 +0200

    Added PetTypes to bootstrap

--- src/main/java/damian/springframework/petclinic/bootstrap/DataLoader.java
+++ src/main/java/damian/springframework/petclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package damian.springframework.petclinic.bootstrap;
 
 import damian.springframework.petclinic.model.Owner;
+import damian.springframework.petclinic.model.PetType;
 import damian.springframework.petclinic.model.Vet;
 import damian.springframework.petclinic.services.OwnerService;
+import damian.springframework.petclinic.services.PetTypeService;
 import damian.springframework.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -11,14 +13,26 @@ import org.springframework.stereotype.Component;
 public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    private final PetTypeService petTypeService;
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
 
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("As");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType snake = new PetType();
+        dog.setName("Długas");
+        PetType savedSnakePetType = petTypeService.save(snake);
+
+
         Owner owner1 = new Owner();
         owner1.setFistName("Krystian");
         owner1.setLastName("Kaminski");
@@ -34,13 +48,13 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded owners");
 
         Vet vet1 = new Vet();
-        vet1.setFistName("as");
-        vet1.setLastName("pieski");
+        vet1.setFistName("ELINOR");
+        vet1.setLastName("MCGRATH");
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setFistName("dlugas");
-        vet2.setLastName("wezowaty");
+        vet2.setFistName("Claude");
+        vet2.setLastName("Bourgelat");
         vetService.save(vet2);
         System.out.println("loaded vets");
 
