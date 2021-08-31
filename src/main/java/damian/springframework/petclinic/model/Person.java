fee1b2f53aa7218aa1e169bd60580939d1575a00
Damianzieba526 <48860148+Damianzieba526@users.noreply.github.com>
2021-08-31 13:37:04 +0200

    Convert Owners to JPA entities

--- src/main/java/damian/springframework/petclinic/model/Person.java
+++ src/main/java/damian/springframework/petclinic/model/Person.java
@@ -1,7 +1,15 @@
 package damian.springframework.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class Person  extends BaseEntity{
+
+    @Column(name = "first_name")
     private String fistName;
+
+    @Column(name = "last_name")
     private String LastName;
 
     public String getFistName() {
