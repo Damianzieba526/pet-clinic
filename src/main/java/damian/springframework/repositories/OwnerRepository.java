0288698bd639d127b4dfc341575e7d7fb7a6dc7c
Damianzieba526 <48860148+Damianzieba526@users.noreply.github.com>
2021-09-03 12:06:39 +0200

    Add spring data jpa owner service

--- src/main/java/damian/springframework/repositories/OwnerRepository.java
+++ src/main/java/damian/springframework/repositories/OwnerRepository.java
@@ -2,7 +2,11 @@ package damian.springframework.repositories;
 
 import damian.springframework.petclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    Owner findByLastName(String lastName);
 
 }
