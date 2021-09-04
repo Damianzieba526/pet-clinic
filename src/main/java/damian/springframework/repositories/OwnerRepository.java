88c8802ffeda60ba1dca3f4b1a4f2527a6d56125
Damianzieba526 <48860148+Damianzieba526@users.noreply.github.com>
2021-09-04 10:33:53 +0200

    Vet service spring data JPA

--- src/main/java/damian/springframework/repositories/OwnerRepository.java
+++ src/main/java/damian/springframework/repositories/OwnerRepository.java
@@ -2,7 +2,6 @@ package damian.springframework.repositories;
 
 import damian.springframework.petclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
