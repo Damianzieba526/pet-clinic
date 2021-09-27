8d8da66d91b759396cdcc3570cad75089c93b3a5
Damianzieba526 <48860148+Damianzieba526@users.noreply.github.com>
2021-09-27 16:07:13 +0200

    added jpa tests for owner service

--- src/main/java/damian/springframework/petclinic/services/OwnerService.java
+++ src/main/java/damian/springframework/petclinic/services/OwnerService.java
@@ -1,8 +1,6 @@
 package damian.springframework.petclinic.services;
 
 import damian.springframework.petclinic.model.Owner;
-import org.springframework.stereotype.Service;
-
 
 public interface OwnerService extends CrudService<Owner, Long>{
     Owner findByLastName(String lastName);
